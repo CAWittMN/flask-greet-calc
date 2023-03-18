46e1d9d1b2b99b43ef2c399c51e79313b7e2639d
Casey Wittrock <caseywittrockmn@gmail.com>
2023-03-18 01:24:16 -0500

    finish greet and calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,18 @@
-# Put your app in here.
+import operations
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/math/<operation>')
+def add(operation):
+    
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    if operation == "add":
+        answer = operations.add(a,b)
+    elif operation == "sub":
+        answer = operations.sub(a,b)
+    elif operation == "mult":
+        answer = operations.mult(a,b)
+    elif operation == "div":
+        answer = operations.div(a,b)
+    return f"{answer}"
